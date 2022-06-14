abea0ac29f5681e46fb5c0285a23f823670ae55f
SamZuBeli <97295077+javabin7777777@users.noreply.github.com>
2022-06-14 11:09:24 -0300

    Add files via upload

--- revelarDestino.js
+++ revelarDestino.js
@@ -17,26 +17,47 @@ const respostas=["sim.","não.","provávelmente.","pode ser.","Certeza!",
 "Sim.",
 "Concentre-se e pergunte novamente.",
 "Sinais apontam que sim."]   // array com as respostas possíveis
+const elementH3=document.querySelector("#resposta")   
+const campoTexto=document.querySelector("#input")
+const elementDiv=document.querySelector("#cronometro")
 
-function perguntar(){   // esta função é executada ao clicar no botão "fazer pergunta"
-    const elementH3=document.querySelector("#resposta")   
-    const campoTexto=document.querySelector("#input")
+function perguntar(){   // esta função é executada ao clicar no botão "fazer pergunta"            
     elementH3.style.opacity = 1
     if(campoTexto.value == ""){  // emite um alerta na tela caso o campo pergunta esteja vazio
         window.alert("Digite algo no campo pergunta")
         return
-    }     
-    elementH3.innerHTML = "<div>"+campoTexto.value+"</div>" + respostas[Math.floor(Math.random()*respostas.length)]
-    campoTexto.value="" 
-    campoTexto.setAttribute("disabled",true)
-    button.setAttribute("disabled",true)          
-    setTimeout(function(){ // despois de 5000ms coloca a opacidade do elemento <h3> para zero
-        elementH3.style.opacity = 0              
-    },5000)
-    setInterval(function(){  // executa a função no intervalo de 8000ms
-        campoTexto.removeAttribute("disabled") 
-        button.removeAttribute("disabled") 
-        elementH3.innerHTML="" 
-    },8000)  
+    }
+    if(campoTexto !=""){
+        cronometro()
+        setTimeout(function(){                     
+            elementH3.innerHTML = "<div>"+campoTexto.value+"</div>" + respostas[Math.floor(Math.random()*respostas.length)]
+            campoTexto.value="" 
+            campoTexto.setAttribute("disabled",true)
+            button.setAttribute("disabled",true)          
+            setTimeout(function(){ // depois de 5000ms coloca a opacidade do elemento <h3> para zero
+                elementH3.style.opacity = 0              
+            },5000)
+            setInterval(function(){  // executa a função no intervalo de 8000ms
+                campoTexto.removeAttribute("disabled") 
+                button.removeAttribute("disabled") 
+                elementH3.innerHTML="" 
+            },8000)
+            elementDiv.innerHTML=""
+
+        },7000)            
+    } 
+}
+
+function cronometro(){   
+    var tempo=5
+    elementDiv.style.opacity=1
+    var id=setInterval(function(){         
+            elementDiv.innerHTML = tempo
+            --tempo
+            if(tempo==0){
+                clearInterval(id)
+                elementDiv.style.opacity=0
+            }
+    },1000)    
 }
 
